d479739a5db6474966171892b02a0de11715d816
yuchen <wangyuchen1120@gmail.com>
2023-07-09 10:21:02 +0800

    nothing new

--- test_cases/test_automation.py
+++ test_cases/test_automation.py
@@ -4,6 +4,7 @@ import yaml
 from components import referral_check_success
 from components.referral_check_success import CheckRetCode
 
+
 class TestClass:
     @pytest.mark.parametrize("test_data", yaml_data_loader("referral_text.yaml"))
     def test_referral_page_text(self, test_data: dict):
