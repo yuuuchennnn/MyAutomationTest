5b61cccd15d018c9c59b25ffc98477372fd722c7
yuuuchennnn <wangyuchen1120@gmail.com>
2023-07-26 20:57:31 +0800

    test

--- test_cases/test_automation.py
+++ test_cases/test_automation.py
@@ -5,6 +5,7 @@ from components import referral_check_success
 from components.referral_check_success import CheckRetCode
 from loguru import logger
 
+
 class TestClass:
     @pytest.mark.parametrize("testData", yaml_data_loader("referral_text.yaml"))
     def test_referral_page_text(self, testData: dict):
