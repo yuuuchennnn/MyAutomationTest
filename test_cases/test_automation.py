28b4b5ec7639367995d1bf91f5aa441013b792fd
yuchen <wangyuchen1120@gmail.com>
2023-08-27 18:33:08 +0800

    mod

--- test_cases/test_automation.py
+++ test_cases/test_automation.py
@@ -7,6 +7,7 @@ from loguru import logger
 
 
 class TestClass:
+
     @pytest.mark.parametrize("testData", yaml_data_loader("referral_text.yaml"))
     def test_referral_page_text(self, testData: dict):
         logger.info(testData)
